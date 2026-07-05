24c12f26582a06dfcb80625ea27820fb469711af
Mustafa <diertojiev@yandex.ru>
2026-07-05 14:13:12 +0300

    .

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -4,7 +4,18 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://leppa-wenston.ru',
+        'https://www.leppa-wenston.ru',
+        'https://xn----7sbhm0aqahda1am8k.xn--p1ai',
+        'https://leppa-wenston.vercel.app',
+      ],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
